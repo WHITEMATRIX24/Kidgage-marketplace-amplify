097431aa99e3e7abca9bf9e1c0541dcfc75f4cd5
Jacob Louis <louisjacob8@gmail.com>
2025-02-07 10:59:48 +0530

    added pwa feature and change in the alignment of top activites arrow btn

--- frontend_v2/src/components/common/header/Header.jsx
+++ frontend_v2/src/components/common/header/Header.jsx
@@ -1,34 +1,34 @@
 import React from "react";
-import logo from '../../../assets/logo.png';
-import UserDetailsPopup from '../../userDetailsPopup/UserDetailsPopup';
+import logo from "../../../assets/logo.png";
+import UserDetailsPopup from "../../userDetailsPopup/UserDetailsPopup";
 import { Col, Row } from "react-bootstrap";
-import './Header.css';
+import "./Header.css";
 import { useNavigate } from "react-router";
 function Header() {
-    const navigate = useNavigate();
-    const handleLogoClick = () => {
-        navigate('/')
-    }
-    return (
-        <div className="Header-container">
-            <Row>
-                <Col md={1} />
-                <Col md={10}>
-                    <div className="header-content">
-                        <img
-                            src={logo}
-                            className="header-logo"
-                            alt="Company Logo"
-                            style={{ cursor: "pointer" }}
-                            onClick={handleLogoClick}
-                        />
-                        <UserDetailsPopup />
-                    </div>
-                </Col>
-                <Col md={2} />
-            </Row>
-        </div>
-    );
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    navigate("/home");
+  };
+  return (
+    <div className="Header-container">
+      <Row>
+        <Col md={1} />
+        <Col md={10}>
+          <div className="header-content">
+            <img
+              src={logo}
+              className="header-logo"
+              alt="Company Logo"
+              style={{ cursor: "pointer" }}
+              onClick={handleLogoClick}
+            />
+            <UserDetailsPopup />
+          </div>
+        </Col>
+        <Col md={2} />
+      </Row>
+    </div>
+  );
 }
 
 export default Header;
--- frontend_v2/vite.config.js
+++ frontend_v2/vite.config.js
@@ -1,9 +1,55 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      // devOptions: {
+      //   enabled: true,
+      // },
+      includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Kidgage",
+        short_name: "kidgage",
+        description: "kidgage",
+        icons: [
+          {
+            src: "/kidgae-fav.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/kidgae-fav.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "favicon",
+          },
+          {
+            src: "/kidgae-fav.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon",
+          },
+          {
+            src: "/kidgae-fav.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       external: ["jspdf"], // Exclude pdfkit from the final bundle
